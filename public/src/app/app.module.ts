8a9968a9d24a1d4f1d91b1196e290106d97bfe52
Alexander Mandera <alexander@mandera.eu>
2019-10-20 16:01:07 +0200

    Beginning of text editor

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -10,6 +10,20 @@ import { ConnectComponent } from './connect/connect.component';
 import { ServerListComponent } from './server-list/server-list.component';
 import { ConsoleComponent } from './console/console.component';
 import { FileTransferComponent } from './file-transfer/file-transfer.component';
+import { EditorComponent } from './editor/editor.component';
+import {HighlightModule} from "ngx-highlightjs";
+
+import xml from 'highlight.js/lib/languages/xml';
+import scss from 'highlight.js/lib/languages/scss';
+import typescript from 'highlight.js/lib/languages/typescript';
+
+export function hljsLanguages() {
+  return [
+    {name: 'typescript', func: typescript},
+    {name: 'scss', func: scss},
+    {name: 'xml', func: xml}
+  ];
+}
 
 @NgModule({
   declarations: [
@@ -19,11 +33,13 @@ import { FileTransferComponent } from './file-transfer/file-transfer.component';
     ConnectComponent,
     ServerListComponent,
     ConsoleComponent,
-    FileTransferComponent
+    FileTransferComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighlightModule.forRoot({ languages: hljsLanguages })
   ],
   providers: [IpcService],
   bootstrap: [AppComponent]
